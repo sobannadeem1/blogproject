a1860488636d5e424b912dd3165229a5a48f01fd
Soban Nadeem <soban312004@gmail.com>
2024-11-28 10:36:57 +0500

    final changes]

--- client/src/components/header/Header.jsx
+++ client/src/components/header/Header.jsx
@@ -1,40 +1,43 @@
-
-import { AppBar, Toolbar, styled, Button } from '@mui/material'; 
-import { Link } from 'react-router-dom';
-
-import { useNavigate } from 'react-router-dom';
-
+import { AppBar, Toolbar, styled } from "@mui/material";
+import { Link } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 
 const Component = styled(AppBar)`
-    background: #FFFFFF;
-    color: black;
+  background: #ffffff;
+  color: black;
 `;
 
 const Container = styled(Toolbar)`
-    justify-content: center;
-    & > a {
-        padding: 20px;
-        color: #000;
-        text-decoration: none;
-    }
-`
+  justify-content: center;
+  & > a {
+    padding: 20px;
+    color: #000;
+    text-decoration: none;
+  }
+`;
 
 const Header = () => {
+  const navigate = useNavigate();
 
-    const navigate = useNavigate();
+  const logout = async () => {
+    // Clear any session storage or authentication tokens
+    sessionStorage.clear();
+    // Redirect the user to the login page
+    navigate("/account");
+  };
 
-    const logout = async () => navigate('/account');
-        
-    return (
-        <Component>
-            <Container>
-                <Link to='/'>HOME</Link>
-                <Link to='/about'>ABOUT</Link>
-                <Link to='/contact'>CONTACT</Link>
-                <Link to='/account'>LOGOUT</Link>
-            </Container>
-        </Component>
-    )
-}
+  return (
+    <Component>
+      <Container>
+        <Link to="/">HOME</Link>
+        <Link to="/about">ABOUT</Link>
+        <Link to="/contact">CONTACT</Link>
+        <Link to="/account" onClick={logout}>
+          LOGOUT
+        </Link>
+      </Container>
+    </Component>
+  );
+};
 
 export default Header;

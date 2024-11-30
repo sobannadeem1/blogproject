d852bc5194598f2cd273253e2f65b46747c7ceb6
Soban Nadeem <soban312004@gmail.com>
2024-11-30 18:46:19 +0500

    kmkm

--- client/src/App.js
+++ client/src/App.js
@@ -1,3 +1,5 @@
+import { useState } from "react";
+
 import { Box } from "@mui/material";
 import {
   BrowserRouter,
@@ -7,7 +9,7 @@ import {
   Outlet,
 } from "react-router-dom";
 
-// Components
+//components
 import DataProvider from "./context/DataProvider";
 import Header from "./components/header/Header";
 import Home from "./components/home/Home";
@@ -17,12 +19,8 @@ import Update from "./components/create/Update";
 import About from "./components/about/About";
 import Contact from "./components/contact/Contact";
 import Login from "./components/account/Login";
-import { useEffect, useState } from "react";
-import AOS from "aos";
-import "aos/dist/aos.css";
 
-// PrivateRoute component to handle authentication
-const PrivateRoute = ({ isAuthenticated }) => {
+const PrivateRoute = ({ isAuthenticated, ...props }) => {
   const token = sessionStorage.getItem("accessToken");
   return isAuthenticated && token ? (
     <>
@@ -35,50 +33,58 @@ const PrivateRoute = ({ isAuthenticated }) => {
 };
 
 function App() {
-  const [isAuthenticated, setIsUserAuthenticated] = useState(false);
-
-  // Initialize AOS (Animate on Scroll) for animations
-  useEffect(() => {
-    AOS.init({
-      duration: 800, // Animation duration in milliseconds
-      easing: "ease-out", // Animation easing
-      once: true, // Whether animation should only happen once
-    });
-  }, []);
+  const [isAuthenticated, isUserAuthenticated] = useState(false);
 
   return (
     <DataProvider>
       <BrowserRouter>
-        <Box sx={{ marginTop: 8 }}>
+        <Box style={{ marginTop: 64 }}>
           <Routes>
-            {/* Public Route for Login */}
             <Route
               path="/account"
-              element={
-                <Login setIsUserAuthenticated={setIsUserAuthenticated} />
-              }
+              element={<Login isUserAuthenticated={isUserAuthenticated} />}
             />
 
-            {/* Private Routes for authenticated users */}
             <Route
               path="/"
               element={<PrivateRoute isAuthenticated={isAuthenticated} />}
             >
-              <Route index element={<Home data-aos="fade-up" />} />
-              <Route
-                path="create"
-                element={<CreatePost data-aos="fade-up" />}
-              />
-              <Route
-                path="details/:id"
-                element={<DetailView data-aos="fade-up" />}
-              />
-              <Route
-                path="update/:id"
-                element={<Update data-aos="fade-up" />}
-              />
-              <Route path="about" element={<About data-aos="fade-up" />} />
-              <Route path="contact" element={<Contact data-aos="fade-up" />} />
+              <Route path="/" element={<Home />} />
+            </Route>
+
+            <Route
+              path="/create"
+              element={<PrivateRoute isAuthenticated={isAuthenticated} />}
+            >
+              <Route path="/create" element={<CreatePost />} />
+            </Route>
+
+            <Route
+              path="/details/:id"
+              element={<PrivateRoute isAuthenticated={isAuthenticated} />}
+            >
+              <Route path="/details/:id" element={<DetailView />} />
+            </Route>
+
+            <Route
+              path="/update/:id"
+              element={<PrivateRoute isAuthenticated={isAuthenticated} />}
+            >
+              <Route path="/update/:id" element={<Update />} />
+            </Route>
+
+            <Route
+              path="/about"
+              element={<PrivateRoute isAuthenticated={isAuthenticated} />}
+            >
+              <Route path="/about" element={<About />} />
+            </Route>
+
+            <Route
+              path="/contact"
+              element={<PrivateRoute isAuthenticated={isAuthenticated} />}
+            >
+              <Route path="/contact" element={<Contact />} />
             </Route>
           </Routes>
         </Box>

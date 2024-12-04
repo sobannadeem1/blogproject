ec72deb7b48c842197e576a8315a84278251cf85
Soban Nadeem <soban312004@gmail.com>
2024-12-04 11:18:28 +0500

    l;

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,4 @@
 import { useState } from "react";
-
 import { Box } from "@mui/material";
 import {
   BrowserRouter,
@@ -9,7 +8,6 @@ import {
   Outlet,
 } from "react-router-dom";
 
-//components
 import DataProvider from "./context/DataProvider";
 import Header from "./components/header/Header";
 import Home from "./components/home/Home";
@@ -20,7 +18,7 @@ import About from "./components/about/About";
 import Contact from "./components/contact/Contact";
 import Login from "./components/account/Login";
 
-const PrivateRoute = ({ isAuthenticated, ...props }) => {
+const PrivateRoute = ({ isAuthenticated }) => {
   const token = sessionStorage.getItem("accessToken");
   return isAuthenticated && token ? (
     <>
@@ -38,7 +36,7 @@ function App() {
   return (
     <DataProvider>
       <BrowserRouter>
-        <Box style={{ marginTop: 64 }}>
+        <Box>
           <Routes>
             <Route
               path="/account"
@@ -49,42 +47,12 @@ function App() {
               path="/"
               element={<PrivateRoute isAuthenticated={isAuthenticated} />}
             >
-              <Route path="/" element={<Home />} />
-            </Route>
-
-            <Route
-              path="/create"
-              element={<PrivateRoute isAuthenticated={isAuthenticated} />}
-            >
-              <Route path="/create" element={<CreatePost />} />
-            </Route>
-
-            <Route
-              path="/details/:id"
-              element={<PrivateRoute isAuthenticated={isAuthenticated} />}
-            >
-              <Route path="/details/:id" element={<DetailView />} />
-            </Route>
-
-            <Route
-              path="/update/:id"
-              element={<PrivateRoute isAuthenticated={isAuthenticated} />}
-            >
-              <Route path="/update/:id" element={<Update />} />
-            </Route>
-
-            <Route
-              path="/about"
-              element={<PrivateRoute isAuthenticated={isAuthenticated} />}
-            >
-              <Route path="/about" element={<About />} />
-            </Route>
-
-            <Route
-              path="/contact"
-              element={<PrivateRoute isAuthenticated={isAuthenticated} />}
-            >
-              <Route path="/contact" element={<Contact />} />
+              <Route index element={<Home />} />
+              <Route path="create" element={<CreatePost />} />
+              <Route path="details/:id" element={<DetailView />} />
+              <Route path="update/:id" element={<Update />} />
+              <Route path="about" element={<About />} />
+              <Route path="contact" element={<Contact />} />
             </Route>
           </Routes>
         </Box>
